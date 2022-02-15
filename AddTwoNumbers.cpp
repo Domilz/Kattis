2d3f79b7a31c084e94894afdc743fadb89f4e98b
Daniel Källberg <Daniel.kallberg@hotmail.com>
2022-02-15 21:49:14 +0100

    solved

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <unordered_map>
+#include <set>
+ 
+void run() {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b;
+    
+}
+ 
+ 
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	
+	run();
+}
